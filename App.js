9c45f6a0cc3c3caf403aec11a3feeb6534961fa4
Shreya <shreya@zkteco.in>
2021-07-29 17:58:52 +0530

    IM-Card changes

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import IMButton from './IMButton';
 import IMCard from './src/IMCard';
-
-
-export default function App() {
+function App() {
   return (
     <View>
       <IMCard/>
@@ -14,11 +11,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
